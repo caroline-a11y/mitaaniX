6f86afce87ccdcd38eff87d9b17c581755719ccc
carol <caroline.syokau@student.moringa.com>
2022-12-16 17:50:37 +0300

    first commit

--- index.js
+++ index.js
@@ -1,67 +1,35 @@
 let shirts = []
 let id = 0;
-fetch (' http://localhost:3000/shirts')
-.then(response => response.json()
-).then(data =>{
-    amountDetailUpdater(data[Math.ceil(math.random()*1000) %data.length])
-    amountUpdata(data)
-})
-function amountUpdater(shirts){
-    for(let amount of shirts){
-        amount(shirts)
-    }
-    function amount(shirts)
-    const amount = document.getElementById('number_of_shirts')
-    let amount = document.createElement('amount')
-        amount.addEventListener('click', event=>{
-            amountDetailUpdater(number)
-        })
-        amount.setAttribute(amount.number)
-        amount.appendChild(amount)
+function dataFetch(){
+    fetch (' http://localhost:3000/shirts')
+    .then(response => response.json()
+    ).then(data => data)
+}
 
-}
-function amountDetailUpdater(number){
-    let amount = document.querySelector("amount")
-    amount.textContent = number.amount
-}
-document.querySelector('form'.addEventListener('submit',(e)=>{
+const form1 = document.getElementById("form1")
+console.log (form1)
+form1.addEventListener("submit", (e) => {
     e.preventDefault()
-    let formData = {
-        number_of_shirts: e.size.color.value
-    }
-    const fetchconfigure = {
-        method : 'POST',
-        headers : {
-            'Content-Type' : 'application/json',
-            'Accept' : 'application/json'
+    const email = e.target.email.value
+    const size =e.target.sizeInput.value
+    const color =e.target.colorInput.value
+    const shirtNo = e.target.shirtInput.value
+    const donation = e.target.donationInput.value
+
+    fetch("http://localhost:3000/shirts", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json"
         },
-        body : json.stringify(formData)
-    }
-    fetch(" http://localhost:3000/shirts", fetchconfigure)
-    .then(response=>{
-        return response.json()
-    }).then(data=>{
-        
+        body: JSON.stringify({
+            "email": email,
+            "size": size,
+            "color": color,
+            "numberOfShirts": shirtNo,
+            "donation": donation
+        })
+       
     })
-}))
-function buyingShirts(shirts){
-
-}
-let button = document.getElementById('buy')
-button.addEventListener('click',()=> {
-    
-})
-function sizes(sizes){
-
-}
-let change = document.getElementById('size')
-change.addEventListener('click', () => {
-
-})
-function colour(colour){
-
-}
-let click = document.getElementById('click')
-click.addEventListener('click', () => {
-
+    form1.reset()
 })
